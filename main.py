7e0a3dc8b4f0d109c99c70887802d2bb8613f6bc
sophie.n <Sophie-nhan@hotmail.com>
2020-08-31 13:55:31 +0200

    commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+
+    print("Hej, mitt namn är Sophie och är uppväxt i Göteborg.")
+    print("Har aldrig programmerat innan, så detta är något helt nytt för mig.")
+    print("Jag sökte till KYH som PVT för att jag var intresserad inom IT och KYH:s hemsida såg mest tilltalande ut.")
+
+
+if __name__ == "__main__":
+    main()
