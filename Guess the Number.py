2271a11e843d08847ade2f28e9c9ecbe83eb29a2
sophie.n <Sophie-nhan@hotmail.com>
2020-09-01 11:14:35 +0200

    Commit

--- Guess the Number.py
+++ Guess the Number.py
@@ -1,23 +1,29 @@
 import random
 
+print("Jag tänker på en siffra mellan 1 och 100, gissa vilken!")
+n = random.randint(1, 100)
 antal = 0
 
-n = random.randint(1, 100)
-print("Jag tänker på en siffra mellan 1 och 100, gissa vilken!")
 
-while True:
-    text = input("Din gissning:")
-    as_number = int(text)
-    antal += 1
+def mainloop():
+    global antal
 
-    if as_number == n:
-        print("Korrekt!")
-        break
+    while True:
+        text = input("Din gissning:")
+        as_number = int(text)
 
-    if as_number < n:
-        print("Fel, min siffra är högre... Testa igen")
+        antal += 1
+        if as_number == n:
+            print("Korrekt!")
+            break
 
-    if as_number > n:
-        print("Fel, min siffra är lägre... Testa igen!")
+        if as_number < n:
+            print("Fel, min siffra är högre... Testa igen")
 
-print("Dina gissningar:", antal)
+        if as_number > n:
+            print("Fel, min siffra är lägre... Testa igen!")
+
+    print("Dina gissningar:", antal)
+
+
+mainloop()
