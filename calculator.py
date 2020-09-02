80650735c1913b2ae434c7e227be0afad7711a1c
sophie.n <Sophie-nhan@hotmail.com>
2020-09-02 10:12:30 +0200

    Commit

--- calculator.py
+++ calculator.py
@@ -26,6 +26,8 @@ def run():
     print("This program will help with simple calculations. What do you want to do?")
     print("1 - add numbers")
     print("2 - subtract numbers")
+    print("3 - multiply numbers")
+    print("4 - divide numbers")
     answer = input(">> ")
     a = int(input("A="))
     b = int(input("B="))
@@ -33,11 +35,14 @@ def run():
         result = add(a, b)
     if answer == "2":
         result = subtract(a, b)
+    if answer == "3":
+        result = multiply(a,b)
+    if answer == "4":
+        result = divide(a,b)
         print("Result = " + str(result))
 
 
 run()
-
 
 if __name__ == "__main__":
     main()
