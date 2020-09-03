33fb88db290a3ece5768cfa5fa08cdd9b76562c7
sophie.n <Sophie-nhan@hotmail.com>
2020-09-03 10:00:57 +0200

    Added: max_value

--- Calc2.py
+++ Calc2.py
@@ -3,11 +3,13 @@ import random
 
 def game():
     correct_answers = 0
-    m = input("Hur många frågeställningar vill du svara på?:")
+    how_many = input("Hur många frågeställningar vill du svara på?:")
+    max_value = input("Högsta siffra?:")
+    max_value = int(max_value)
 
-    for i in range(int(m)):
-        a = random.randint(1, 10)
-        b = random.randint(1, 10)
+    for i in range(int(how_many)):
+        a = random.randint(1,max_value)
+        b = random.randint(1,max_value)
         answer = input(f"{a}+{b}")
         number = int(answer)
 
@@ -17,9 +19,8 @@ def game():
         else:
             print(f"Fel... Det blir {a + b}")
             print("---")
-    print(f"Du fick {correct_answers} av {m} rätt.")
+    print(f"Du fick {correct_answers} av {how_many} rätt.")
 
 
 if __name__ == '__main__':
     game()
-
