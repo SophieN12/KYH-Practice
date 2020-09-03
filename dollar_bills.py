83c8af9d839432e3482d768c4e12ad622a033fcf
sophie.n <Sophie-nhan@hotmail.com>
2020-09-03 14:15:44 +0200

    Exercise "dollar bills" (Typo)

--- dollar_bills.py
+++ dollar_bills.py
@@ -1,12 +1,12 @@
 def main():
-    notes ={"1": "George Washinton",
-           "2": "Thomas Jefferson",
-           "5": "Abraham Lincoln",
-           "10": "Alexander Hamilton",
-           "20": "Andrew Jackson",
-           "50": "Ulysses S. Grant",
-           "100": "Benjamin Franklin"
-    }
+    notes = {"1": "George Washington",
+             "2": "Thomas Jefferson",
+             "5": "Abraham Lincoln",
+             "10": "Alexander Hamilton",
+             "20": "Andrew Jackson",
+             "50": "Ulysses S. Grant",
+             "100": "Benjamin Franklin"
+             }
     value = (input("Type in the value of an US dollar bill:"))
 
     if value not in notes:
