135abd073fa027f30fae2ef1fed68cc21ad26ab2
sophie.n <Sophie-nhan@hotmail.com>
2020-09-03 12:32:57 +0200

    Everything included

--- uppg10.py
+++ uppg10.py
@@ -1,8 +1,24 @@
-def main():
+print("10.1")
+for i in range(1,11):
+    print(i)
 
-    for i in reversed(range(1, 11)):
-        print(i)
+print("10.2")
+for i in range(1,50,2):
+    print(i)
 
+print("10.3")
+for i in reversed(range(1,11)):
+    print(i)
 
-if __name__ == "__main__":
-    main()
+print("10.4")
+total = 0
+for i in range(7, 1001):
+    total += i
+print(total)
+
+print("10.5")
+product = 1
+for i in range(1,1001):
+    product *= 1
+print(product)
+
