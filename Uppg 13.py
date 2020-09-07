e4651ac8ace8624b27742709bb478628bfc37f87
sophie.n <Sophie-nhan@hotmail.com>
2020-09-07 14:46:20 +0200

    Exercise 13, better/easier way to do it.

--- Uppg 13.py
+++ Uppg 13.py
@@ -1,25 +1,28 @@
 import random
 
-text = str("En app som kan laddas ned till mobiltelefonen ska varna finländare som kommit nära någon som smittats med "
-           "coronaviruset. – Jag tycker att ni i Sverige borde överväga att göra något liknande, "
-           "säger MarkkuTervahauta, generaldirektör för Finska institutet för hälsa och välfärd, THL.").split(" ")
-
 kate1 = ("svensken", "norsken")
 kate2 = ("spanish flu", "vattenkoppor")
 kate3 = ("Sophie", "Alexander")
 kate4 = ("pressekreterare", "chef")
 
-a = random.randint(0, len(kate1)-1)
-text[10] = kate1[a]
+kate1 = random.choice(kate1)
+kate2 = random.choice(kate2)
+kate3 = random.choice(kate3)
+kate4 = random.choice(kate4)
 
-b = random.randint(0, len(kate2)-1)
-text[18] = kate2[b]
+text = str(f"En app som kan laddas ned till mobiltelefonen ska varna {kate1} som kommit nära någon som smittats med {kate2}. – Jag tycker att ni i Sverige borde överväga att göra något liknande, säger {kate3}, {kate4} för Finska institutet för hälsa och välfärd, THL.")
 
-c = random.randint(0, len(kate3)-1)
-text[33] = kate3[c]
 
-d = random.randint(0, len(kate4)-1)
-text[34] = kate4[d]
+
+
+
+
+
+
+
+
+
+
 
 
 print(text)
