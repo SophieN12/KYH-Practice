a0310a92d55d3870b9a12cba3ab2b9f99a22bb62
sophie.n <Sophie-nhan@hotmail.com>
2020-09-21 15:11:50 +0200

    Uppgift 30 & 31 (Full)

--- slicing.py
+++ slicing.py
@@ -8,11 +8,16 @@ def main():
 
     int_list = [int(number)for number in numbers]
 
-    print(f"Första talet: {numbers[0]}\nSista talet: {numbers[-1]}\n"
-          f"Summan av talen: {sum(int_list)}")
-    numbers.reverse()
-    numbers1 = ", ".join(numbers)
-    print(f"Talen baklänges: {numbers1}")
+    reversed_numbers = list(reversed(numbers))
+    numbers = ", ".join(numbers)
+    reversed_numbers = ", ".join(reversed_numbers)
+
+    print(f"Första talet: {numbers[0]}\n"
+          f"Sista talet: {numbers[-1]}\n"
+          f"Summan av talen: {sum(int_list)}\n"
+          f"Talen baklänges: {reversed_numbers}\n"
+          f"Udda tal: {numbers[0:100:2]}\n"
+          f"Jämna tal: {numbers[1:100:2]}")
 
 
 if __name__ == "__main__":
