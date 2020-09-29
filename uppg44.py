b5532b78aaf86fdaeb4b9b33bbcc8c464f9ee46e
sophie.n <Sophie-nhan@hotmail.com>
2020-09-29 14:08:58 +0200

    Uppgift 44

--- uppg44.py
+++ uppg44.py
@@ -1,12 +1,7 @@
-# Uppgift 44
-
 def ask_user(user):
     (name, age) = user
     print(f"Namn: {name}")
     print(f"Ålder: {age}")
-
-
-ask_user(user=("Sophie", 20))
 
 
 def change_place(t):
@@ -16,14 +11,7 @@ def change_place(t):
     return t
 
 
-t = (16,23)
-print(change_place(t))
-
-
 def convert(ls):
     return tuple(ls)
 
 
-ls = ["Sophie", "Kevin", "Nancy"]
-print(type(convert(ls)))
-print(convert(ls))
