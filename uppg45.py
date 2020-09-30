7e4208e8eaa31f7c40b85ea5a12468f767c479a2
sophie.n <Sophie-nhan@hotmail.com>
2020-09-30 10:20:14 +0200

    Uppgift 45.3

--- uppg45.py
+++ uppg45.py
@@ -4,14 +4,14 @@ def main():
               'Lisa': '9999999999',
               'Bodil': '555 - 666 - 789'}
     while True:
-        print("Antal personer i telefonkatalogen: %s" % (len(people)))
+        print("Antal personer i telefonkatalogen: %20s" % (len(people)))
         choice = input("1: Slå upp ett nummer, 2: Redigera/lägg till nummer, 3: Avsluta:")
 
         if choice == "1":
             vem = input('Vem vill du ringa?')
             if vem in people:
                 number = people[vem]
-                print('Numret till %s är %s' % (vem, number))
+                print('Numret till %20s är %20s' % (vem, number))
                 break
             else:
                 print('Sorry hörru, vet ej vem detta är. Har endast VIP i min katalog')
@@ -20,7 +20,7 @@ def main():
             name = input("Skriv namn:")
             number1 = input("Skriv nummer:")
             people[name] = number1
-            print("%s är nu inlagt i telefonkatalogen" % name)
+            print("%20s är nu inlagt i telefonkatalogen" % name)
 
         elif choice == "3":
             break
